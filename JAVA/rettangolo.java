cb9fb9bf7d8c9b942e05494cb201212093c6acc2
TIBIO117 <153857496+TIBIO117@users.noreply.github.com>
2025-01-10 08:21:11 +0000

    salva

---
+++ JAVA/rettangolo.java
@@ -0,0 +1,15 @@
+class rettangolo 
+    private int base;
+    private in altezza;
+
+    public setBase (int base) {
+        this.base = base;
+    }
+
+    public int Area () {
+        return base * altezza;
+    }
+}
+public class rettangolo {
+    
+}
