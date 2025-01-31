a8bdc8f0cfa10eafff149d542a00506aa3bf86c2
TIBIO117 <153857496+TIBIO117@users.noreply.github.com>
2025-01-31 08:15:15 +0000

    salva

---
+++ JAVA/EREDITARIETA'/Forma.java
@@ -0,0 +1,28 @@
+/*Crea una classe base Forma con i seguenti attributi e metodi:
+Attributi: nome (una stringa che descrive il tipo di forma, ad esempio "Cerchio", "Rettangolo").
+Metodi:
+calcolaArea(): restituisce 0 (sarà sovrascritto nelle sottoclassi).
+calcolaPerimetro(): restituisce 0 (sarà sovrascritto nelle sottoclassi).
+descrizione(): stampa il nome della forma. */
+
+private class Forma {
+    
+    private String nome;
+
+    public Forma(String nome) {
+        this.nome = nome;
+    }
+
+    public double calcolaArea() {
+        return 0;
+    }
+
+    public double calcolaPerimetro() {
+        return 0;
+    }
+
+    public String Descrizione() {
+        System.out.println("forma: "+ nome);
+        return null;
+    }
+}
